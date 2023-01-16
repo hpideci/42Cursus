0df903cf6c3b64f876e0cff9eec147e2d01649fc
Husnu Can Pideci <hpideci@k2m11s03.42kocaeli.com.tr>
2023-01-16 13:13:36 +0300

    letsssgooo

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hpideci <42kocaeli.com.tr>                 +#+  +:+       +#+        */
+/*   By: hpideci <@student.42kocaeli.com.tr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 03:11:09 by hpideci           #+#    #+#             */
-/*   Updated: 2023/01/08 03:15:40 by hpideci          ###   ########.tr       */
+/*   Updated: 2023/01/16 13:07:50 by hpideci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
 	size_t	i;
-	size_t	j;
+	size_t	c;
+	size_t	n_len;
 
-	if (!*needle)
-		return ((char *)haystack);
 	i = 0;
-	while (haystack[i] == needle[0])
+	n_len = ft_strlen(needle);
+	if (haystack == NULL)
+		return (NULL);
+	if (n_len == 0 || haystack == needle)
+		return ((char *)haystack);
+	while (((char *)(haystack))[i] != '\0' && i < len)
 	{
-		if (haystack[i] == needle[0])
-		{
-			j = 0;
-			while (haystack[i + j] == needle[j] && i + j < len)
-			{
-				if (needle[j + 1] == '\0')
-					return ((char *)&haystack[i]);
-				j++;
-			}
-		}
+		c = 0;
+		while (((char *)(haystack))[i + c] != '\0' && needle[c] != '\0'
+			&& ((char *)(haystack))[i + c] == needle[c] && i + c < len)
+			c++;
+		if (c == n_len)
+			return ((char *)(haystack + i));
 		i++;
 	}
 	return (0);
