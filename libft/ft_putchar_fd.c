958fd42734a3c778eb7d27616d7751fede24f0cc
Husnu Can Pideci <hpideci@k2m03s08.42kocaeli.com.tr>
2023-02-07 11:37:23 +0300

    putchar_fd is fixed

--- libft/ft_putchar_fd.c
+++ libft/ft_putchar_fd.c
@@ -6,22 +6,13 @@
 /*   By: hpideci <@student.42kocaeli.com.tr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 13:30:38 by hpideci           #+#    #+#             */
-/*   Updated: 2023/01/16 18:37:18 by hpideci          ###   ########.fr       */
+/*   Updated: 2023/02/07 11:36:49 by hpideci          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <fcntl.h>
-#include <stdio.h>
 
 void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
-}
-
-int main()
-{
-	int fd = open("a.txt", 1);
-	ft_putchar_fd('d', fd);
-	
 }
